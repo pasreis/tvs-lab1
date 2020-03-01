34247479dc55db6e7545001629618bbaa848bb0b
pasreis <pedro.dos.reis@tecnico.ulisboa.pt>
2020-03-01 16:48:53 +0000

    Added tests for method divide

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,6 +4,86 @@ import static org.testng.Assert.*;
 import org.testng.annotations.*;
 
 public class CalculatorTest {
+	@DataProvider
+	private Object[][] getSumValues() {
+		return new Object[][] {
+			{ 1, 1, 2 },
+			{ 0, 1, 1 },
+			{ 0, 0, 0 },
+			{ -1, -1, -2 },
+			{ -1,  1,  0 },
+			{  1, -1,  0 },
+			{ -1,  0, -1 },
+			{  0, -1, -1 },
+			{ Integer.MAX_VALUE, Integer.MAX_VALUE, -2 },
+			{ Integer.MIN_VALUE, Integer.MIN_VALUE,  0 },
+			{ Integer.MAX_VALUE, Integer.MIN_VALUE, -1 },
+			{ Integer.MIN_VALUE, Integer.MAX_VALUE, -1 },
+			{ Integer.MAX_VALUE,  1, Integer.MIN_VALUE },
+			{ Integer.MAX_VALUE, -1, Integer.MAX_VALUE - 1 },
+			{ Integer.MAX_VALUE,  0, Integer.MAX_VALUE },
+			{ Integer.MAX_VALUE, null, Integer.MAX_VALUE },
+			{  1, Integer.MAX_VALUE, Integer.MIN_VALUE },
+			{ -1, Integer.MAX_VALUE, Integer.MAX_VALUE - 1 },
+			{  0, Integer.MAX_VALUE, Integer.MAX_VALUE },
+			{ null, Integer.MAX_VALUE, Integer.MAX_VALUE },
+			{ Integer.MIN_VALUE,  1, Integer.MIN_VALUE + 1 },
+			{ Integer.MIN_VALUE, -1, Integer.MAX_VALUE },
+			{ Integer.MIN_VALUE,  0, Integer.MIN_VALUE },
+			{ Integer.MIN_VALUE, null, Integer.MIN_VALUE },
+			{  1, Integer.MIN_VALUE, Integer.MIN_VALUE + 1 },
+			{ -1, Integer.MIN_VALUE, Integer.MAX_VALUE },
+			{  0, Integer.MIN_VALUE, Integer.MIN_VALUE },
+			{ null, Integer.MIN_VALUE, Integer.MIN_VALUE },
+			{ null, null, 0 },
+			{ null, 1, 1 },
+			{ 1, null, 1 },
+			{ null, -1, -1 },
+			{ -1, null, -1 }
+		};
+	}
+
+	@DataProvider
+	private Object[][] getDivideExceptionValues() {
+		return new Object[][] {
+			{  2, 0 },
+			{ -2, 0 },
+			{  0, 0 },
+			{ Integer.MAX_VALUE, 0 },
+			{ Integer.MIN_VALUE, 0 },
+			{ Integer.MAX_VALUE, null },
+			{ Integer.MIN_VALUE, null },
+			{ null, 0 },
+			{  4, null },
+			{ -4, null },
+			{  0, null }
+		};
+	}
+
+	@DataProvider
+	private Object[][] getDivideValues() {
+		return new Object[][] {
+			{  4,  2,  2 },
+			{ -4,  2, -2 },
+			{  0,  2,  0 },
+			{  4, -2, -2 },
+			{ -4, -2,  2 },
+			{  0, -2,  0 },
+			{  4, Integer.MAX_VALUE,  4 / Integer.MAX_VALUE },
+			{ -4, Integer.MAX_VALUE, -4 / Integer.MAX_VALUE },
+			{  0, Integer.MAX_VALUE, 0 },
+			{  4, Integer.MIN_VALUE,  4 / Integer.MIN_VALUE },
+			{ -4, Integer.MIN_VALUE, -4 / Integer.MIN_VALUE },
+			{  0, Integer.MIN_VALUE,  0 / Integer.MIN_VALUE },
+			{ Integer.MAX_VALUE, Integer.MAX_VALUE,  1 },
+			{ Integer.MIN_VALUE, Integer.MIN_VALUE,  1 },
+			{ Integer.MAX_VALUE, Integer.MIN_VALUE,  0 },
+			{ Integer.MIN_VALUE, Integer.MAX_VALUE, -1 },
+			{ null,  2, 0 },
+			{ null, -2, 0 }
+		};
+	}
+
 	@Test(groups = { "constructor" })
 	public void testConstructorWithNameOK() {
 		// Arrange
@@ -85,40 +165,11 @@ public class CalculatorTest {
 		assertEquals(calculator.getName(), name);
 	}
 
-	@DataProvider
-	private Object[][] getSumValues() {
-		return new Object[][] {
-			{ 1, 1, 2 },
-			{ 0, 1, 1 },
-			{ 0, 0, 0 },
-			{ -1, -1, -2 },
-			{ -1,  1,  0 },
-			{  1, -1,  0 },
-			{ -1,  0, -1 },
-			{  0, -1, -1 },
-			{ Integer.MAX_VALUE, Integer.MAX_VALUE, -2 },
-			{ Integer.MIN_VALUE, Integer.MIN_VALUE,  0 },
-			{ Integer.MAX_VALUE, 1, Integer.MIN_VALUE },
-			{ 1, Integer.MAX_VALUE, Integer.MIN_VALUE },
-			{ Integer.MAX_VALUE, -1, Integer.MAX_VALUE - 1 },
-			{ -1, Integer.MAX_VALUE, Integer.MAX_VALUE - 1 },
-			{ Integer.MIN_VALUE, 1, Integer.MIN_VALUE + 1 },
-			{ 1, Integer.MIN_VALUE, Integer.MIN_VALUE + 1 },
-			{ Integer.MIN_VALUE, -1, Integer.MAX_VALUE },
-			{ -1, Integer.MIN_VALUE, Integer.MAX_VALUE },
-			{ null, null, 0 },
-			{ null, 1, 1 },
-			{ 1, null, 1 },
-			{ null, -1, -1 },
-			{ -1, null, -1 }
-		};
-	}
-
 	@Test(
 		groups = { "sum" },
 		dataProvider = "getSumValues")
 	public void testSum(Integer a, Integer b, Integer expectedResult) {
-		// Assert
+		// Arrange
 		Calculator calculator = new Calculator("Tejo");
 
 		// Act
@@ -126,5 +177,36 @@ public class CalculatorTest {
 
 		// Assert
 		assertEquals(result, expectedResult);
+		assertEquals(calculator.getNumberOfOperations(), 1);
+	}
+
+	@Test(
+		groups = { "divide" },
+		dataProvider = "getDivideExceptionValues")
+	public void testDivideExceptions(Integer a, Integer b) {
+		// Arrange
+		IllegalArgumentException exception;
+		Calculator calculator = new Calculator("Tejo");
+
+		// Act
+		exception = expectThrows(IllegalArgumentException.class, () -> { calculator.divide(a, b); });
+
+		// Assert
+		assertTrue(exception.getMessage().contains("b is zero"));
+	}
+
+	@Test(
+		groups = { "divide" },
+		dataProvider = "getDivideValues")
+	public void testDivide(Integer a, Integer b, Integer expectedResult) {
+		// Arrange
+		Calculator calculator = new Calculator("Tejo");
+
+		// Act
+		Integer result = calculator.divide(a, b);
+
+		// Assert
+		assertEquals(result, expectedResult);
+		assertEquals(calculator.getNumberOfOperations(), 1);
 	}
 }
