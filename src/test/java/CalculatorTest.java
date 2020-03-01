e01d15039695baa4033c4f265bfc192de57003b5
pasreis <pedro.dos.reis@tecnico.ulisboa.pt>
2020-03-01 15:50:36 +0000

    Added tests for method sum

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -84,4 +84,47 @@ public class CalculatorTest {
 		assertNotNull(calculator);
 		assertEquals(calculator.getName(), name);
 	}
+
+	@DataProvider
+	private Object[][] getSumValues() {
+		return new Object[][] {
+			{ 1, 1, 2 },
+			{ 0, 1, 1 },
+			{ 0, 0, 0 },
+			{ -1, -1, -2 },
+			{ -1,  1,  0 },
+			{  1, -1,  0 },
+			{ -1,  0, -1 },
+			{  0, -1, -1 },
+			{ Integer.MAX_VALUE, Integer.MAX_VALUE, -2 },
+			{ Integer.MIN_VALUE, Integer.MIN_VALUE,  0 },
+			{ Integer.MAX_VALUE, 1, Integer.MIN_VALUE },
+			{ 1, Integer.MAX_VALUE, Integer.MIN_VALUE },
+			{ Integer.MAX_VALUE, -1, Integer.MAX_VALUE - 1 },
+			{ -1, Integer.MAX_VALUE, Integer.MAX_VALUE - 1 },
+			{ Integer.MIN_VALUE, 1, Integer.MIN_VALUE + 1 },
+			{ 1, Integer.MIN_VALUE, Integer.MIN_VALUE + 1 },
+			{ Integer.MIN_VALUE, -1, Integer.MAX_VALUE },
+			{ -1, Integer.MIN_VALUE, Integer.MAX_VALUE },
+			{ null, null, 0 },
+			{ null, 1, 1 },
+			{ 1, null, 1 },
+			{ null, -1, -1 },
+			{ -1, null, -1 }
+		};
+	}
+
+	@Test(
+		groups = { "sum" },
+		dataProvider = "getSumValues")
+	public void testSum(Integer a, Integer b, Integer expectedResult) {
+		// Assert
+		Calculator calculator = new Calculator("Tejo");
+
+		// Act
+		Integer result = calculator.sum(a, b);
+
+		// Assert
+		assertEquals(result, expectedResult);
+	}
 }
